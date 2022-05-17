64eb198d91d1521c98b89dae453e1d006e9f7e04
Varvara Lipunova <varvaralipunova@Varvaras-MacBook-Air.local>
2022-05-16 18:51:31 -0700

    add divider, segment and placeholder components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [{path: '', component: HomeComponent}, {path: '**', component: NotFoundComponent}];
+const routes: Routes = [{path: 'elements', loadChildren: () => import('./elements/elements.module').then((m)=>m.ElementsModule)},{path: 'collections', loadChildren: () => import('./collections/collections.module').then((m)=>m.CollectionsModule)},{path: '', component: HomeComponent}, {path: '**', component: NotFoundComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsTabComponent } from './collections-tab/collections-tab.component';
 
-const routes: Routes = [{path:'collections', component:CollectionsTabComponent}];
+const routes: Routes = [{path:'', component:CollectionsTabComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
